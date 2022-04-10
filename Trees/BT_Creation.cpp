26b3884e50789aa18db412fd2059bc5160d449d3
Prateek Mittal <prateekmittal154@gmail.com>
2022-04-10 16:15:46 +0530

    commit

---
+++ Trees/BT_Creation.cpp
@@ -0,0 +1,39 @@
+# include<bits/stdc++.h>
+
+using namespace std;
+
+class node{
+
+public: 
+
+	int data;
+	node* left;
+	node* right;
+
+
+	node(int data){
+
+		this -> data = data;
+		this-> left= NULL;
+		this-> right=NULL;
+	}
+};
+
+node* buildtree(){
+
+	cout<<"Enter the data: "<<endl;
+
+	int data;
+
+	cin>>data
+
+
+}
+
+int main(){
+
+
+	node* root=NULL;
+
+	root=buildtree();
+}
